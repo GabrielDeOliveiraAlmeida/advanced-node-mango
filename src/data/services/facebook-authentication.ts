01e1eb74235048f710ff63d623f30f92d42d6e44
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-06-11 16:30:19 -0300

    change SUT creation strategy to use beforeEach

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,6 +1,6 @@
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
-import { LoadFacebookUserApi } from '@/data/contracts/apis'
 
 export class FacebookAuthenticationService {
   constructor (private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -3,14 +3,18 @@ import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('FaceboookAuthentication', () => {
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let sut: FacebookAuthenticationService
+
+  beforeEach(() => {
+    loadFacebookUserApi = mock<LoadFacebookUserApi>()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  })
+
   it('should call LoadFacebookAPI with correct params', async () => {
-    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
-
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
-
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
@@ -18,11 +22,7 @@ describe('FaceboookAuthentication', () => {
   })
 
   it('should return AuthenticationError  when LoadFAcebookAPI return undefined', async () => {
-    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
-
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
-
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     const result = await sut.perform({ token: 'any_token' })
 
