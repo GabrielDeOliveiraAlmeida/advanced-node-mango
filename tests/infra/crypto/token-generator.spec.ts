35f441c1de114df266b5a47f826f9b8a5939aed1
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-06-13 22:22:47 -0300

    isolate sut creation

--- tests/infra/crypto/token-generator.spec.ts
+++ tests/infra/crypto/token-generator.spec.ts
@@ -12,13 +12,23 @@ export class JwtTokenGenerator {
   }
 }
 describe('JwtTokenGenerator', () => {
-  it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+  let secret: string
 
+  beforeAll(() => {
+    secret = 'any_secret'
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator(secret)
+  })
+
+  it('should call sign with correct params', async () => {
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', {
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, secret, {
       expiresIn: 1
     })
   })
