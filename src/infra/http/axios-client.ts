d2c8511fdc8552add93704a2afb0da6f6e50c7df
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-06-13 22:32:31 -0300

    jwt token generator implments interface

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -2,7 +2,7 @@ import { HttpGetClient } from '@/infra/http'
 
 import axios from 'axios'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get (args: HttpGetClient.Params): Promise<any> {
     const reuslt = await axios.get(args.url, { params: args.params })
     return reuslt.data
