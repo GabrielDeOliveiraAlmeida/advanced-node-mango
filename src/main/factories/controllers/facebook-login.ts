a2a525524156e2cd182f605f9ea6772b4e57026e
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-08-19 23:37:00 -0300

    transform Express class to a function

--- src/main/factories/controllers/facebook-login.ts
+++ src/main/factories/controllers/facebook-login.ts
@@ -2,7 +2,7 @@
 import { FacebookLoginController } from '@/application/controllers'
 import { makeFacebookAuthenticationService } from '@/main/factories/services/facebook-authentication'
 
-export const makeFacebookController = (): FacebookLoginController => {
+export const makeFacebookLoginController = (): FacebookLoginController => {
   const fbServices = makeFacebookAuthenticationService()
   return new FacebookLoginController(fbServices)
 }
