e6fdbcf0507ec8c1103cf59f0146001e854875d1
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-08-21 15:33:58 -0300

    rename TokenGenerator to TokenHandler

--- src/infra/crypto/token-handler.ts
+++ src/infra/crypto/token-handler.ts
@@ -5,8 +5,9 @@ import jwt from 'jsonwebtoken'
 type Params = TokenGenerator.Params
 type Result = TokenGenerator.Result
 
-export class JwtTokenGenerator implements TokenGenerator {
+export class JwtTokenHandler implements TokenGenerator {
   constructor (private readonly secret: string) {}
+
   async generateToken ({ expirationInMs, key }: Params): Promise<Result> {
     const expirationInSeconds = expirationInMs / 1000
     return jwt.sign({ key }, this.secret, { expiresIn: expirationInSeconds })
