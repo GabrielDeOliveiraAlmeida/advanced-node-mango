121ff835368ee1123dfce4114781ec62233e19ec
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-08-21 15:19:00 -0300

    ensure authorize return accesstoken

--- tests/domain/use-cases/authorize.spec.ts
+++ tests/domain/use-cases/authorize.spec.ts
@@ -1,18 +1,20 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 interface TokenValidator {
-  validateToken: (params: TokenValidator.Params) => Promise<void>
+  validateToken: (params: TokenValidator.Params) => Promise<TokenValidator.Result>
 }
 
 export namespace TokenValidator {
   export type Params = {token: string}
+  export type Result = string
 }
 
 type Setup = (crypto: TokenValidator) => Authorize
 type Input = { token: string}
-type Authorize = (params: Input) => Promise<void>
+type Output = string
+type Authorize = (params: Input) => Promise<Output>
 const setupAuthorize: Setup = (crypto) => async params => {
-  await crypto.validateToken(params)
+  return await crypto.validateToken(params)
 }
 
 describe('Authorize ', () => {
@@ -23,16 +25,23 @@ describe('Authorize ', () => {
   beforeAll(() => {
     token = 'any_token'
     crypto = mock()
+    crypto.validateToken.mockResolvedValue('any_value')
   })
 
   beforeEach(() => {
     sut = setupAuthorize(crypto)
   })
 
-  it('should call LoadFacebookAPI with correct params', async () => {
+  it('should Authorize calls TokenValidator with correct params', async () => {
     await sut({ token })
 
     expect(crypto.validateToken).toHaveBeenCalledWith({ token })
     expect(crypto.validateToken).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return the correct accesToken', async () => {
+    const userId = await sut({ token })
+
+    expect(userId).toBe('any_value')
   })
 })
