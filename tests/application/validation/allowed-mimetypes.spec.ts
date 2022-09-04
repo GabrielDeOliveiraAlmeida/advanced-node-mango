dad678883e813ece16ca5f7b5b71dd88f91faa88
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-09-04 17:36:12 -0300

    ensure AllowedMimeTypes return error if value is invalid

---
+++ tests/application/validation/allowed-mimetypes.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidMimeTypeError } from '@/application/errors'
+
+type Extensions = 'png' | 'jpg'
+
+class AllowedMimeTypes {
+  constructor (private readonly allowed: Extensions[], private readonly mimeType: string) {}
+
+  validate (): Error {
+    return new InvalidMimeTypeError(this.allowed)
+  }
+}
+
+describe('AllowedMimeTypes', () => {
+  it('should  return InvalidMimeTypeError if value is invalid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/png')
+
+    const error = sut.validate()
+    expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+})
