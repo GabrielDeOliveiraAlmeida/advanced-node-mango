17aacb98f0930135eed740f2d2a53b73f7132404
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-09-04 17:38:29 -0300

    ensure AllowedMimeTypes return return undefined if value is valid

--- tests/application/validation/allowed-mimetypes.spec.ts
+++ tests/application/validation/allowed-mimetypes.spec.ts
@@ -5,16 +5,27 @@ type Extensions = 'png' | 'jpg'
 class AllowedMimeTypes {
   constructor (private readonly allowed: Extensions[], private readonly mimeType: string) {}
 
-  validate (): Error {
-    return new InvalidMimeTypeError(this.allowed)
+  validate (): Error | undefined {
+    if (this.allowed.includes('png') && this.mimeType !== 'image/png') {
+      return new InvalidMimeTypeError(this.allowed)
+    }
   }
 }
 
 describe('AllowedMimeTypes', () => {
   it('should  return InvalidMimeTypeError if value is invalid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/jpg')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+
+  it('should  return undefined if value is valid', () => {
     const sut = new AllowedMimeTypes(['png'], 'image/png')
 
     const error = sut.validate()
-    expect(error).toEqual(new InvalidMimeTypeError(['png']))
+
+    expect(error).toBeUndefined()
   })
 })
