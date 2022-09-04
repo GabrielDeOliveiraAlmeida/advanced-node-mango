326ddedfa6b7b9e9154478e76d15fa4f014b25be
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-09-04 14:24:41 -0300

    ensure AWSS3FileStorage calls deleteObject with correct input

--- tests/infra/gateways/aws-s3.spec.ts
+++ tests/infra/gateways/aws-s3.spec.ts
@@ -89,9 +89,27 @@ describe('AWSS3FileStorage', () => {
     })
   })
 
-  describe(' ', () => {
-    it('should ', () => {
+  describe('delete', () => {
+    let deleteObjectPromiseSpy: jest.Mock
+    let deleteObjectSpy: jest.Mock
 
+    beforeAll(() => {
+      deleteObjectPromiseSpy = jest.fn()
+      deleteObjectSpy = jest.fn().mockImplementation(() => ({ promise: deleteObjectPromiseSpy }))
+      mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({
+        deleteObject: deleteObjectSpy
+      })))
+    })
+
+    it('should call deleteObject with correct input', async () => {
+      await sut.delete({ key })
+
+      expect(deleteObjectSpy).toHaveBeenCalledWith({
+        Bucket: bucket,
+        Key: key
+      })
+      expect(deleteObjectSpy).toHaveBeenCalledTimes(1)
+      expect(deleteObjectPromiseSpy).toHaveBeenCalledTimes(1)
     })
   })
 })
