1149699ebec07fea474c08930446593588252256
GabrieldeOliveiraAlmeida <gabriel.doliveira.almeida@gmail.com>
2022-09-07 14:24:02 -0300

    ensure PUT /user/picture returns 200 on success

--- tests/main/routes/user-routes.spec.ts
+++ tests/main/routes/user-routes.spec.ts
@@ -48,6 +48,12 @@ describe('User Routes ', () => {
   })
 
   describe('PUT /users/picture', () => {
+    const uploadSpy = jest.fn()
+
+    jest.mock('@/infra/gateways/aws-s3-file-storage', () => ({
+      AwsS3FileStorage: jest.fn().mockReturnValue({ upload: uploadSpy })
+    }))
+
     it('should return 403 with no authorization header present', async () => {
       const { status } = await request(app)
         .put('/api/users/picture')
@@ -55,16 +61,18 @@ describe('User Routes ', () => {
       expect(status).toBe(403)
     })
 
-    // it('should return 200 with valid data', async () => {
-    //   const { id } = await pgUserRepo.save({ email: 'any_email', name: 'Gabriel almeida' })
-    //   const authorization = sign({ key: id }, env.jwtSecret)
+    it('should return 200 with valid data', async () => {
+      uploadSpy.mockResolvedValueOnce('any_url')
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'Gabriel almeida' })
+      const authorization = sign({ key: id }, env.jwtSecret)
 
-    //   const { status, body } = await request(app)
-    //     .delete('/api/users/picture')
-    //     .set({ authorization })
+      const { status, body } = await request(app)
+        .put('/api/users/picture')
+        .set({ authorization })
+        .attach('picture', Buffer.from('any_buffer'), { filename: 'any_name', contentType: 'image/png' })
 
-    //   expect(status).toBe(200)
-    //   expect(body).toEqual({ pictureUrl: undefined, initials: 'GA' })
-    // })
+      expect(status).toBe(200)
+      expect(body).toEqual({ pictureUrl: 'any_url', initials: undefined })
+    })
   })
 })
